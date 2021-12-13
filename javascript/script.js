800434bddbf2b7a8b92b2c16a5da580ccee082a4
dmkirshon <kirshon.david@gmail.com>
2021-12-13 12:16:16 +0200

    feat: Add prompt for user input for num of boxes

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,34 @@
 const containerDiv = document.querySelector('.container');
 const clearButton = document.querySelector('.clearButton');
+const containerWidth = containerDiv.offsetWidth;
 const defaultColor = 'grey';
 const filledColor = 'aqua';
+let gridSize;
+let boxLength;
+
+// prompt to ask user how big the grid should be, don't make too many boxes
+// so set max to 100 on one side
+function promptGridSize() {
+    do{
+        gridSize = prompt('Please the width of the grid (max 100):', '64');
+    
+        if (0 >= gridSize || gridSize >= 101) {
+            alert('Please enter a number between 1 and 100. Thank you!');
+        }
+    }  
+    while(0 >= gridSize || gridSize >= 101)
+
+
+boxLength = containerWidth/gridSize;
+
 
 // create boxs in container to create etch pad
 
-for(i = 0; i < 16; i++) {
+for(i = 0; i < gridSize*gridSize; i++) {
     let newDiv = document.createElement('div');
     newDiv.classList.toggle('box');
+    newDiv.style.width = `${boxLength}px`;
+    newDiv.style.height = `${boxLength}px`;
     containerDiv.appendChild(newDiv);
 }
 
@@ -21,7 +42,7 @@ containerDiv.childNodes.forEach(box => {
 function fillColor() {
     this.style.backgroundColor = filledColor;
 }
-
+}
 // clear button to default back to a clear grid
 
 clearButton.addEventListener('click', clearGrid);
@@ -29,6 +50,8 @@ clearButton.addEventListener('click', clearGrid);
 function clearGrid() {
     containerDiv.childNodes.forEach(box => {
         box.style.backgroundColor = defaultColor;
-        console.log('clear');
     });
+    promptGridSize();
 }
+
+promptGridSize();
