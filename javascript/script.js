381602f9abf63a77710ea71e868e11fb1e893ab7
dmkirshon <kirshon.david@gmail.com>
2021-12-13 10:45:09 +0200

    docs: Add comments for reason beind code

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,15 @@
 const containerDiv = document.querySelector('.container');
 
+// create boxs in container to create etch pad
+
 for(i = 0; i < 16; i++) {
     let newDiv = document.createElement('div');
     newDiv.classList.toggle('box');
     containerDiv.appendChild(newDiv);
 }
+
+
+// etch boxes with a color when hovered over to create pixel art
 
 containerDiv.childNodes.forEach(box => {
     box.addEventListener('mouseover', changeColor);
