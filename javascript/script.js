508709cb5e6e6cc3a61917e359dca2f80ea49d27
dmkirshon <kirshon.david@gmail.com>
2021-12-15 15:35:21 +0200

    feat: Add toggle between hover and click etch

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,11 @@
+/**
+ * Script: Etch-a-sketch 
+ * Author: David Kirshon
+ * Date:Dec 15, 2021
+ * This script allows for etching of the grid as well as controls
+ * that bring more functionality to the user.
+ */
+
 const containerDiv = document.querySelector('.container');
 const clearButton = document.querySelector('.clear-button');
 const etchStyleButton = document.querySelector('.etch-style-button');
@@ -6,37 +14,36 @@ const defaultColor = 'grey';
 const filledColor = 'aqua';
 let gridSize = 0;
 let boxLength = 0;
+let clickEtchOptionSelected = false;
 
 
 // Run initial grid creation  
-
 function init() {
-    promptGridSize();
+    promptGridSize();block comments
     createBoxes();
     etchBoxes();
 };
 init();
 
 // Prompt user for Grid width, max 100 blocks
-
 function promptGridSize() {
-    do{
+    do {
         gridSize = prompt('Please the width of the grid (max 100):', '64');
-    
+
         if (0 >= gridSize || gridSize >= 101) {
             alert('Please enter a number between 1 and 100. Thank you!');
         }
-    }  
-    while(0 >= gridSize || gridSize >= 101)
+    }
+    while (0 >= gridSize || gridSize >= 101);
 
-    boxLength = containerWidth/gridSize;
+    boxLength = containerWidth / gridSize;
 }
 
 
 
 // create boxs in container to create etch pad
 function createBoxes() {
-    for(i = 0; i < gridSize*gridSize; i++) {
+    for (i = 0; i < gridSize * gridSize; i++) {
         let newDiv = document.createElement('div');
         newDiv.classList.toggle('box');
         newDiv.setAttribute('draggable', false);
@@ -48,7 +55,7 @@ function createBoxes() {
 
 
 // etch boxes with a color when hovered over to create pixel art
-function etchBoxes () {
+function etchBoxes() {
     containerDiv.childNodes.forEach(box => {
         box.addEventListener('mouseover', fillColor);
     });
@@ -59,7 +66,6 @@ function fillColor() {
 }
 
 // clear button to default back to a clear grid
-
 clearButton.addEventListener('click', clearGrid);
 
 function clearGrid() {
@@ -69,17 +75,29 @@ function clearGrid() {
     init();
 }
 
-// Change etch style from mouse over to mouse down for easier etching for user
 
+// Change the etch style between hover etch and click to etch.
 etchStyleButton.addEventListener('click', changeEtchStyle);
 
+
 function changeEtchStyle() {
-    containerDiv.childNodes.forEach(box => {
-        box.removeEventListener('mouseover', fillColor);
-        box.addEventListener('mousedown', clickEtchBoxes);
-        box.addEventListener('mouseup', stopEtchBoxes);
-    });
-    
+    clickEtchOptionSelected = !(clickEtchOptionSelected);
+    if (clickEtchOptionSelected) {
+        etchStyleButton.textContent = 'Hover-to-Etch';
+        containerDiv.childNodes.forEach(box => {
+            box.removeEventListener('mouseover', fillColor);
+            box.addEventListener('mousedown', clickEtchBoxes);
+            box.addEventListener('mouseup', stopEtchBoxes);
+        });
+    } else {
+        etchStyleButton.textContent = 'Click-to-Etch';
+        etchBoxes();
+        containerDiv.childNodes.forEach(box => {
+            box.removeEventListener('mousedown', clickEtchBoxes);
+            box.removeEventListener('mouseup', stopEtchBoxes);
+        });
+    }
+
 }
 
 
